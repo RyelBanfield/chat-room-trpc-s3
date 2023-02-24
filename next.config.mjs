826a7ad9b02630dd1019e6b193fdb0188ab31187
Ryel Banfield <ryelbanfield@gmail.com>
2023-02-24 02:44:52 -0400

    Add AWS domain to next config

--- next.config.mjs
+++ next.config.mjs
@@ -20,5 +20,8 @@ const config = {
     locales: ["en"],
     defaultLocale: "en",
   },
+  images: {
+    domains: ["chat-room-trcp-s3-images.s3.us-east-2.amazonaws.com"],
+  },
 };
 export default config;
