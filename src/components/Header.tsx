badcd3d80b7ea26bdb497d8afe82571059f9939a
Ryel Banfield <ryelbanfield@gmail.com>
2023-02-24 02:45:45 -0400

    Create Header

---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <header className="container mx-auto border-b-2 p-6">
+      <h1 className="text-4xl font-semibold">Ryel&apos;s Room - TRPC - S3</h1>
+    </header>
+  );
+};
+
+export default Header;
